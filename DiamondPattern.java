b98a581af259765cdcd3c41ca7171df6465d5500
Hasnain-Sidd <110290157+Hasnain-Sidd@users.noreply.github.com>
2022-11-26 06:06:25 -0800

    Add files via upload

---
+++ DiamondPattern.java
@@ -0,0 +1,28 @@
+public class DiamondPattern {
+    public static void main(String[] args) {
+     
+ 
+     for(int i=1; i<=5; i++) 
+     {
+         for(int j=5; j>i; j--) {
+             System.out.print(" ");
+         }
+         for(int k=1; k<=(2*i-1); k++) {
+             System.out.print("*");
+         }
+         System.out.print("\n");
+         }
+     for(int i=4; i>=1; i--) 
+     {
+         for(int j=5; j>i; j--) {
+             System.out.print(" ");
+             }
+         for(int k=1; k<=(2*i-1); k++) {
+             System.out.print("*");
+             }
+         System.out.print("\n");
+         }
+     }
+    }
+ 
+ 
