bdae98cb1d6c0b96cc6e3182ce7d8f09b151777d
Hasnain Hissam <110290157+Hasnain-Sidd@users.noreply.github.com>
2023-01-16 21:34:07 +0500

    Add files via upload

---
+++ Array_List_Stack.java
@@ -0,0 +1,59 @@
+import java.io.PushbackInputStream;
+import java.util.ArrayList;
+import java.util.Random;
+
+public class Array_List_Stack {
+   static class Stack{
+    static ArrayList<Integer> L=new ArrayList<>();
+    public Boolean isEmpty(){
+        return (L.size()==0);
+    }
+    public void Push(int data){
+        L.add(data);
+    }
+    public int Pop(){
+        int start=L.get(L.size()-1);
+        L.remove(L.size()-1);
+        return start;
+    }
+    public int Peek(){
+        int start=L.get(L.size()-1);
+        return start;
+    }
+    public void pushATBottom(int data,Stack S){
+        if(S.isEmpty()){
+            S.Push(data);
+            return;
+        }
+        int top=S.Pop();
+        S.pushATBottom(data, S);
+      S.Push(top);
+    }
+    public static void Reverse(Stack S){
+        if(S.isEmpty()){
+            return;
+        }
+        int start =S.Pop();
+        Reverse(S);
+        S.pushATBottom(start, S);
+    }
+   } 
+   public static void main(String[] args) {
+    Stack S=new Stack();
+    Random R=new Random();
+    S.Push(8);
+    S.Push(89);
+    S.Push(78);
+    S.Push(190);
+    S.Push(56);
+    S.pushATBottom(18, S);
+   S.Reverse(S);
+    System.out.print("Elements In The Stack : ");
+      System.out.print("{");
+    while(S.isEmpty()!=true){
+        System.out.print(S.Peek()+",");
+        S.Pop();
+    }
+     System.out.print("}");
+   }
+}
